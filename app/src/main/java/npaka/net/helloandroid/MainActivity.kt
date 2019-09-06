f6e87157218d6707c59c51f4eb58a43983a78e2a
TakumiTSK <tata1205TSK@gmail.com>
2019-09-06 16:03:29 +0900

    chapter3提出

--- app/src/main/java/npaka/net/helloandroid/MainActivity.kt
+++ app/src/main/java/npaka/net/helloandroid/MainActivity.kt
@@ -1,12 +1,18 @@
-package npaka.net.helloandroid
+package com.example.enpit_p38.helloandroid
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import npaka.net.helloandroid.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapHere.setOnClickListener{
+            textView.text = "ボタンがタップされました"
+        }
     }
 }
